7ed93ee2cd4549bbd3fbad7e073441539f4a96cf
Ioannis Aliazis <ioannisaliazis@me.com>
2018-12-23 10:57:37 +0200

    new icon

--- wp-login.php
+++ wp-login.php
@@ -17,10 +17,10 @@
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="/wp-content/uploads/2018/12/cropped-icon-32x32.png" sizes="32x32" />
-<link rel="icon" href="/wp-content/uploads/2018/12/cropped-icon-192x192.png" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2018/12/cropped-icon-180x180.png" />
-<meta name="msapplication-TileImage" content="/wp-content/uploads/2018/12/cropped-icon-270x270.png" />
+	<link rel="icon" href="/wp-content/uploads/2018/12/cropped-icon-1-32x32.png" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2018/12/cropped-icon-1-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2018/12/cropped-icon-1-180x180.png" />
+<meta name="msapplication-TileImage" content="/wp-content/uploads/2018/12/cropped-icon-1-270x270.png" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
